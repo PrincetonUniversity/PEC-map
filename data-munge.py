b27e61c1c220aee91658df81b6b33015273448c5
Hope Johnson <hopecaneel@gmail.com>
2020-06-09 15:26:42 -0400

    update with state data

--- data-munge.py
+++ data-munge.py
@@ -20,13 +20,16 @@ senate = pd.read_csv("/Users/hopecj/projects/PEC/PEC-map/data/PEC Map Data 2020 
 stleg = pd.read_csv("/Users/hopecj/projects/PEC/PEC-map/data/PEC Map Data 2020  - State Legislatures.csv")
 supreme = pd.read_csv("/Users/hopecj/projects/PEC/PEC-map/data/PEC Map Data 2020  - State Supreme Court.csv")
 
-# Shapefiles of US congressional districts and US states
+# Shapefiles
 cong_shp = gpd.read_file("/Users/hopecj/projects/PEC/PEC-map/data/cb_2019_us_cd116_500k/cb_2019_us_cd116_500k.shp")
+state_shp = gpd.read_file("/Users/hopecj/projects/PEC/PEC-map/data/cb_2018_us_state_500k/cb_2018_us_state_500k.shp")
 
-
-# State code to state FIPS crosswalk
+# State-code-to-state-FIPS crosswalk
 fips = pd.read_csv("/Users/hopecj/Downloads/StateFIPSicsprAB.csv")
 
+################################
+#  CREATE CONGRESSIONAL DATA   #
+################################
 cong = cong.merge(fips, left_on='State', right_on='AB')
 
 cd_code = cong["District"].str.split(pat = "-", expand=True)
@@ -50,3 +53,36 @@ out = out[["NAME", "District", "Code", "D", "R", "April Cook Ratings",
 # next: merge with full congressional map in QGIS
 # then save as geojson
 out.to_file("cong_dat.shp")
+
+###############################
+#      CREATE STATE DATA      #
+###############################
+senate['State'] = senate['State'].str.replace(' ', '')
+gov['State'] = gov['State'].str.replace(' ', '')
+stleg['State'] = stleg['State'].str.replace(' ', '')
+
+
+st_data = senate.merge(gov, how='outer')
+st_data = st_data.merge(stleg, how='outer')
+st_data = st_data.merge(redistrict, how='outer')
+st_data = st_data.merge(referenda, how='outer')
+st_data = st_data.merge(supreme, how='outer')
+list(st_data.columns)
+st_data['STUSPS'] = st_data['State']
+st_data.groupby(['STUSPS']).agg(['count']) 
+# for now, filter out GA special
+st_data = st_data.drop([7])
+
+st_out = state_shp.merge(st_data, on='STUSPS')
+st_out = st_out[['STATEFP', 'State', 'Senate Special', 'Senate D',
+       'Senate R', 'Senate Cook Rating April', 'Senate Comments',
+       'Senate Cook Rating June', 'Governor D', 'Governor R',
+       'Governor Opposition Primary', 'Governor Cook Rating April',
+       'Governor Comments', 'Governor Cook Rating June', 'State House',
+       'State Senate', 'State Legislature April CNalysis rating',
+       'State Legislature Comments', 'State Legislature June CNalysis rating',
+       'PGP Link', 'Ballot Measures Include', 'Ballotpedia Link',
+       'State Supreme Court Elections', 'State Supreme Court Ballotpedia Link',
+       'State Supreme Court Retention', 'State Supreme Court Comments', 'geometry']]
+st_out.to_file("state_dat.shp")
+
