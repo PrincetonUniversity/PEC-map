3c8f5b5377b55eb5e45a6230c1f4a40150bcd1f6
Hope Johnson <hopecaneel@gmail.com>
2020-06-12 15:26:46 -0400

    update layer

--- main.js
+++ main.js
@@ -7,68 +7,80 @@ const map = new mapboxgl.Map({
 
 map.on('load', function() {
     // Add a source for the state polygons.
-    map.addSource('states', {
-    'type': 'geojson',
-    'data': './out-files/state_dat_june11.geojson'
+    map.addSource('PEC-map', {
+        'type': 'vector',
+        'url': 'mapbox://openprecincts.PEC-map'
     });
 
-    // this can be used to add state label 
+    map.addLayer(
+        {
+            'id': 'state',
+            'source': 'PEC-map',
+            'source-layer': 'state',
+            'minzoom': 0,
+            'maxzoom': 5,
+            'paint': {
+            // 'fill-color': 'rgba(200, 100, 240, 0.4)',
+            'fill-opacity': 0,
+                },
+            'type': 'fill',
+        }
+    );
+
+    map.addLayer(
+        {
+            'id': 'congressional',
+            'source': 'PEC-map',
+            'source-layer': 'congressional',
+            'minzoom': 5,
+            'paint': {
+                // 'fill-color': 'rgba(200, 200, 140, 0.4)',
+                'fill-opacity': 0,
+                    },
+            'type': 'fill',
+        }
+    );
+
+
+
+
+
+
+
+
+
+
+    // // Need to add a layer so that states are click-y
     // map.addLayer({
-    //     id: "state-name",
-    //     type: "symbol",
-    //     source: "states",
-    //     layout: {
-    //         "text-field": "{STATEFP}\n",
-    //         // "text-font": ["Open Sans"],
-    //         "text-size": 12,
-    //         'symbol-placement': "point"
-    //     },
-    //     paint: {
-    //         "text-color": ["case",
-    //             ["boolean", ["feature-state", "hover"], false],
-    //             'rgba(255,0,0,0.75)',
-    //             'rgba(0,0,0,0.75)'
-    //         ],
-    //         "text-halo-color": ["case",
-    //             ["boolean", ["feature-state", "hover"], false],
-    //             'rgba(255,255,0,0.75)',
-    //             'rgba(255,255,255,0.75)'
-    //         ],
-    //         "text-halo-width": 2,
-    //         "text-halo-blur": 0,
+    //     'id': 'states-layer',
+    //     'type': 'fill',
+    //     'source': 'states',
+    //     'maxzoom': zoomThreshold,
+    //     'paint': {
+    //         'fill-color': 'rgba(200, 100, 240, 0.4)',
+    //         'fill-opacity': 0,
     //     }
     // });
 
-    // Need to add a layer so that states are click-y
-    map.addLayer({
-        'id': 'states-layer',
-        'type': 'fill',
-        'source': 'states',
-        'paint': {
-            'fill-color': 'rgba(200, 100, 240, 0.4)',
-            'fill-opacity': 0,
-        }
-    });
+    // map.on('click', 'states-layer', function(e) {
+    //     let el = document.createElement('div');
+    //     el.className = 'marker';
 
-    map.on('click', 'states-layer', function(e) {
-        let el = document.createElement('div');
-        el.className = 'marker';
+    //     new mapboxgl.Popup(el)
+    //         .setLngLat(e.lngLat)
+    //         .setHTML(e.features[0].properties.STATEFP)
+    //         .setHTML('<h3>' + e.features[0].properties.State + '</h3><p>' + e.features[0].properties["Senate Cook Rating June"] + '</p>')
+    //         .addTo(map);
+    //     });
 
-        new mapboxgl.Popup(el)
-            .setLngLat(e.lngLat)
-            .setHTML(e.features[0].properties.STATEFP)
-            .setHTML('<h3>' + e.features[0].properties.State + '</h3><p>' + e.features[0].properties["Senate Cook Rating June"] + '</p>')
-            .addTo(map);
-        });
-
-    map.on('mouseenter', 'states-layer', function() {
-        map.getCanvas().style.cursor = 'pointer';
-    });
+    // map.on('mouseenter', 'states-layer', function() {
+    //     map.getCanvas().style.cursor = 'pointer';
+    // });
         
-        // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'states-layer', function() {
-         map.getCanvas().style.cursor = '';
-    });
+    //     // Change it back to a pointer when it leaves.
+    // map.on('mouseleave', 'states-layer', function() {
+    //      map.getCanvas().style.cursor = '';
+    // });
 
 
 });
