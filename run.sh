337ce78dc44ff8d792d1c64fb8aacb4ae3bf548e
Hope Johnson <hopecaneel@gmail.com>
2020-06-24 16:19:43 -0400

    new version of MB tiles recipe

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 # with "out-files" as working directory 
 
 export MAPBOX_ACCESS_TOKEN=mytoken
-tilesets add-source openprecincts congressional ./house_dat_june11.geojson
+tilesets add-source openprecincts congressFill ./house_dat_june24.geojson
+tilesets add-source openprecincts congressBoundry ./house_boundaries.geojson
 tilesets add-source openprecincts state ./state_dat_june11.geojson
-tilesets create openprecincts.PEC-map --recipe ../recipe.json --name "PEC map V1"
+tilesets create openprecincts.PEC-mapV2 --recipe ../recipe.json --name "PEC map V2"
 
-tilesets publish openprecincts.PEC-map 
+tilesets publish openprecincts.PEC-mapV2 
 
-tilesets status openprecincts.PEC-map # to check the status
+tilesets status openprecincts.PEC-mapV2 # to check the status
