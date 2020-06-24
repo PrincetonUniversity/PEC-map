b19162211a5256d6b67fe316e1b8fe3c3eda5206
Hope Johnson <hopecaneel@gmail.com>
2020-06-24 16:16:59 -0400

    update data cleaning script

--- data-munge.py
+++ data-munge.py
@@ -50,8 +50,9 @@ cong_out = cong_shp.merge(cong, on='Code')
 cong_out = cong_out[["NAME", "District", "Code", "D", "R", "April Cook Ratings",
            "June Cook Ratings", "Opposition Primary", "geometry"]]
 
-# next: upload this file and full congressional shapefile as separate sources to generate MB tiles
+# next: upload these files and full congressional shapefile as separate sources to generate MB tiles
 cong_out.to_file("/Users/hopecj/projects/PEC/PEC-map/out-files/house_dat_june24.geojson", driver="GeoJSON")
+cong_shp.to_file("/Users/hopecj/projects/PEC/PEC-map/out-files/house_boundaries.geojson", driver="GeoJSON")
 
 
 ###############################
