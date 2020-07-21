ce19714c4d87ca518178b6cd0091bb70c64a0a79
Hope Johnson <hopecaneel@gmail.com>
2020-07-21 14:38:09 -0400

    multi-layer work

--- main.js
+++ main.js
@@ -115,6 +115,10 @@ map.on('load', function() {
                     ]
                 },
             'type': 'fill',
+            'layout': {
+                // make layer visible by default
+                'visibility': 'visible'
+                },        
         }
     );
 
@@ -262,6 +266,39 @@ map.on('load', function() {
     );
 
     // add plus/minus zoom button
-    map.addControl(new mapboxgl.NavigationControl());
+    // map.addControl(new mapboxgl.NavigationControl());
+
+    // enumerate ids of the layers
+    var toggleableLayerIds = ['congressional-layer'];
+    
+    // set up the corresponding toggle button for each layer
+    for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+    
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+    
+    link.onclick = function(e) {
+    var clickedLayer = this.textContent;
+    e.preventDefault();
+    e.stopPropagation();
+    
+    var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+    
+    // toggle layer visibility by changing the layout object's visibility property
+    if (visibility === 'visible') {
+    map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+    this.className = '';
+    } else {
+    this.className = 'active';
+    map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+    }
+    };
+    
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+    }
 
 });
